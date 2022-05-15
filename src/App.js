331f0be83a9bee2641345b4b9bfc8a01bfe06f66
Alius <aliusbosas@gmail.com>
2022-05-15 21:28:36 +0300

    Fixed

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <Router>
-      <div className='flex justify-between flex-col bg-yellow-500 '>
+      <div className='flex flex-col justify-between h-screen bg-yellow-500 '>
         <Navbar />
-        <main className=''> Content </main>
+        <main className='text-black text-xl items-center flex justify-center '>Content</main>
+        <Footer />
       </div>
-
    </Router>
 
   );
