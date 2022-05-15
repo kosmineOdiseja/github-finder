3f324b895b5a91ca827a69604a7e859948342cef
Alius <aliusbosas@gmail.com>
2022-05-15 20:21:12 +0300

    Navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -11,7 +11,17 @@ const Navbar = ({ title }) => {
 					<FaGithub className='inline pr-2 text-3xl' />
 					<Link to='/' className='text-lg font-bold align-middle'>{title}</Link>
 				</div>
-				
+				<div className='flex-1 px-2 mx-2'>
+					<div className='flex justify-end'>
+						<Link to='/' className='text btn-ghost btn-sm rounded-btn'>
+							Home
+						</Link>
+						<Link to='/about' className='text btn-ghost btn-sm rounded-btn'>
+							About 
+						</Link>
+					</div>
+				</div>
+
 			</div>
 		</nav>
 
