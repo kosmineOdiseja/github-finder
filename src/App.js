e9e81dd277eb898cdf710de5eaebba49cad529e1
Alius <aliusbosas@gmail.com>
2022-05-15 20:11:08 +0300

    Navbar component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
-    <>
-     <h1 className="text-3xl font-bold text-color-red underline">
-        Hello world!
-      </h1>
-      <button className="btn btn-outline btn-success">
-        Click me
-      </button>
-   </>
+    <Router>
+      <div className='flex justify-between flex-col bg-yellow-500 '>
+        <Navbar />
+        <main className=''> Content </main>
+      </div>
+
+   </Router>
 
   );
 }
