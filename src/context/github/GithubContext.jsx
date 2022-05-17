d3bdcc4b83654278f7b8d4beb8501df5a668a1a5
Alius <aliusbosas@gmail.com>
2022-05-17 13:38:48 +0300

    reworkign code, solving .env file issue cuz Github

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -1,5 +1,8 @@
 import { createContext, useReducer } from "react";
 import  GithubReducer  from './GithubReducer'
+import { ACTIONS } from './GithubReducer'
+
+console.log(ACTIONS, 'this is actions object from githubReducer');
 
 const GithubContext = createContext();
 
@@ -10,12 +13,14 @@ export const GithubProvider = ({ children }) => {
 
 	const InitialState =  {
 		users: [],
-		loading: true, 
+		loading: false, 
 	}
 
 	const [state, dispach ] =useReducer(GithubReducer, InitialState);
 
+	// get initial users ( testing purposes )
 	const fetchUsers = async () => {
+		setLoading();
     const response = await fetch(`${GITHUB_URL}/users`,
       {
         method: 'GET',
@@ -28,10 +33,17 @@ export const GithubProvider = ({ children }) => {
     const data = await response.json();
 
 	dispach({
-		type: 'GET_USERS',
+		type: ACTIONS.GET_USERS, 
 		payload: data
 	})
   }
+
+  const setLoading = () => {
+	dispach({
+	  type: ACTIONS.SET_LOADING, 
+	})
+  }
+
 
   return (
 	<GithubContext.Provider
--- src/context/github/GithubReducer.jsx
+++ src/context/github/GithubReducer.jsx
@@ -1,13 +1,25 @@
+export const ACTIONS = {
+	GET_USERS: 'GET_USERS',
+	SET_LOADING: 'SET_LOADING',
+}
+
 const githubReducer = ( state, action ) => {
+
+
 	// you can use if statement, but the problem is that you have to a lot of if statements 
 	// so convention it is to use switch statement
 	switch ( action.type ) {
 
-		case 'GET_USERS':
+		case ACTIONS.GET_USERS: 
 			return {
 				...state,
 				users: action.payload, 
 				loading: false, 
+			}
+		case ACTIONS.SET_LOADING: 
+			return {
+				...state,
+				loading: true,
 			}
 		default: 
 		return state;
